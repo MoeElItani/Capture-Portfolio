3ba0c8ffbe38b129605942d111262cc6c5b03ddc
MoeElItani <mhmd98itani@gmail.com>
2022-07-21 16:48:50 +0300

    About section

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutSection from '../components/AboutSection'
+
+const AboutUs = () => {
+   return (
+      <div>
+         <AboutSection />
+         {/* <FaqSection /> */}
+         {/* <ServicesSection/> */}
+      </div>
+   )
+}
+
+export default AboutUs
