45706242e23f0d63b400a4392869f92792c0aa69
MoeElItani <mhmd98itani@gmail.com>
2022-07-21 17:03:49 +0300

    Services Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
 
 const AboutUs = () => {
    return (
-      <div>
+      <>
          <AboutSection />
+         <ServicesSection/>
          {/* <FaqSection /> */}
-         {/* <ServicesSection/> */}
-      </div>
+      </>
    )
 }
 
