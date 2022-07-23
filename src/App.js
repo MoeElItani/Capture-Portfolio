05da94be6f16c52a1d2aa30da32a088b68b1a039
MoeElItani <mhmd98itani@gmail.com>
2022-07-23 14:29:05 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // Global Style
 import GlobalStyle from './components/GlobalStytle'
 // Imports
-import AboutUs from './pages/AboutUs'
 import Nav from './components/Nav'
+import AboutUs from './pages/AboutUs'
+import OurWork from './components/OurWork'
+import ContactUs from './components/ContactUs'
+// Router
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
    return (
       <div className='App'>
          <GlobalStyle />
          <Nav />
-         <AboutUs />
+         <Routes>
+            <Route path='/' element={<AboutUs />} />
+            <Route path='/work' element={<OurWork />} />
+            <Route path='/contact' element={<ContactUs />} />
+         </Routes>
       </div>
    )
 }
