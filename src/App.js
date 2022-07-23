c7b6c0dd19bf7c046aedd9809e348d55ec3e0307
MoeElItani <mhmd98itani@gmail.com>
2022-07-23 13:32:08 +0300

    Nabar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import GlobalStyle from './components/GlobalStytle'
 // Imports
 import AboutUs from './pages/AboutUs'
+import Nav from './components/Nav'
 
 function App() {
    return (
       <div className='App'>
          <GlobalStyle />
+         <Nav />
          <AboutUs />
       </div>
    )
