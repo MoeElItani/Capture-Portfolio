f8442e1a5fdcda14cf2b3fd274e6c0092dd4ac0d
MoeElItani <mhmd98itani@gmail.com>
2022-11-07 16:48:22 +0200

    minor fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
 
 const Nav = () => {
    const { pathname } = useLocation()
-
    return (
       <StyledNav>
          <h1>
-            <Link id='Logo' to='/'>
+            <Link id='logo' to='/'>
                Capture
             </Link>
          </h1>
@@ -33,18 +30,21 @@ const Nav = () => {
                   transition={{ duration: 0.75 }}
                   initial={{ width: '0%' }}
                   animate={{
-                     width: pathname === '/work' ? '50%' : '0%',
+                     width:
+                        pathname === '/work' ? '50%' : '0%',
                   }}
                />
             </li>
             <li>
-               <Link to='/contact'>3. Contact us</Link>
+               <Link to='/contact'>3. Contact Us</Link>
                <Line
                   transition={{ duration: 0.75 }}
                   initial={{ width: '0%' }}
                   animate={{
                      width:
-                        pathname === '/contact' ? '50%' : '0%',
+                        pathname === '/contact'
+                           ? '50%'
+                           : '0%',
                   }}
                />
             </li>
@@ -60,20 +60,20 @@ const StyledNav = styled.nav`
    justify-content: space-between;
    align-items: center;
    padding: 1rem 10rem;
-   background-color: #282828;
+   background: #282828;
    position: sticky;
    top: 0;
-   z-index: 999;
+   z-index: 10;
    a {
-      color: #fff;
+      color: white;
       text-decoration: none;
    }
    ul {
       display: flex;
       list-style: none;
    }
-   #Logo {
-      font-size: 1.8rem;
+   #logo {
+      font-size: 1.5rem;
       font-family: 'Lobster', cursive;
       font-weight: lighter;
    }
@@ -86,7 +86,7 @@ const StyledNav = styled.nav`
       padding: 2rem 1rem;
       #logo {
          display: inline-block;
-         margin: 2rem;
+         margin: 1rem;
       }
       ul {
          padding: 2rem;
@@ -98,6 +98,7 @@ const StyledNav = styled.nav`
       }
    }
 `
+
 const Line = styled(motion.div)`
    height: 0.3rem;
    background: #23d997;
@@ -106,122 +107,8 @@ const Line = styled(motion.div)`
    bottom: -80%;
    left: 60%;
    @media (max-width: 1300px) {
-      left: 30%;
+      left: 0%;
    }
 `
 
 export default Nav
-=======
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import { useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-
-const Nav = () => {
-   const { pathname } = useLocation()
-
-   return (
-      <StyledNav>
-         <h1>
-            <Link id='Logo' to='/'>
-               Capture
-            </Link>
-         </h1>
-         <ul>
-            <li>
-               <Link to='/'>1. About Us</Link>
-               <Line
-                  transition={{ duration: 0.75 }}
-                  initial={{ width: '0%' }}
-                  animate={{
-                     width: pathname === '/' ? '50%' : '0%',
-                  }}
-               />
-            </li>
-            <li>
-               <Link to='/work'>2. Our Work</Link>
-               <Line
-                  transition={{ duration: 0.75 }}
-                  initial={{ width: '0%' }}
-                  animate={{
-                     width: pathname === '/work' ? '50%' : '0%',
-                  }}
-               />
-            </li>
-            <li>
-               <Link to='/contact'>3. Contact us</Link>
-               <Line
-                  transition={{ duration: 0.75 }}
-                  initial={{ width: '0%' }}
-                  animate={{
-                     width:
-                        pathname === '/contact' ? '50%' : '0%',
-                  }}
-               />
-            </li>
-         </ul>
-      </StyledNav>
-   )
-}
-
-const StyledNav = styled.nav`
-   min-height: 10vh;
-   display: flex;
-   margin: auto;
-   justify-content: space-between;
-   align-items: center;
-   padding: 1rem 10rem;
-   background-color: #282828;
-   position: sticky;
-   top: 0;
-   z-index: 999;
-   a {
-      color: #fff;
-      text-decoration: none;
-   }
-   ul {
-      display: flex;
-      list-style: none;
-   }
-   #Logo {
-      font-size: 1.8rem;
-      font-family: 'Lobster', cursive;
-      font-weight: lighter;
-   }
-   li {
-      padding-left: 10rem;
-      position: relative;
-   }
-   @media (max-width: 1300px) {
-      flex-direction: column;
-      padding: 2rem 1rem;
-      #logo {
-         display: inline-block;
-         margin: 2rem;
-      }
-      ul {
-         padding: 2rem;
-         justify-content: space-around;
-         width: 100%;
-         li {
-            padding: 0;
-         }
-      }
-   }
-`
-const Line = styled(motion.div)`
-   height: 0.3rem;
-   background: #23d997;
-   width: 0%;
-   position: absolute;
-   bottom: -80%;
-   left: 60%;
-   @media (max-width: 1300px) {
-      left: 30%;
-   }
-`
-
-export default Nav
->>>>>>> b1f3dc0a37a4d785b98661b388d9ce93df63c197
